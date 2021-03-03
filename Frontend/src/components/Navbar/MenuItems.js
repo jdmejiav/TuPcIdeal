8347e7ef6b111b93f026100d5b7f534fbb97ad3e
Juan Diego <jdmejiav@eafit.edu.co>
2021-03-03 17:03:18 -0500

    NavBar component

---
+++ Frontend/src/components/Navbar/MenuItems.js
@@ -0,0 +1,19 @@
+export const MenuItems = {
+    {
+        tittle: 'Home',
+        url :'#',
+        cName : 'nav-links',
+    },{
+        tittle: 'informacion-basica',
+        url :'#',
+        cName : 'nav-links',
+    },{
+        tittle: 'Log in',
+        url :'#',
+        cName : 'nav-links',
+    },{
+        tittle: 'Sign up',
+        url :'#',
+        cName : 'nav-links',
+    },
+}
---
+++ Frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import react, {Component} from 'react';
+import {MenuItems} from './MenuItems';
+import './Navbar.css'
+
+
+class Navbar extends Component{
+    
+    state={
+        clicked: false
+    }
+
+
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">TuPCIdeal.com<i className="far fa-lightbulb"></i></h1>                
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul>
+                    {
+                        MenuItems.map((item,index)=>{
+                            return(
+                                <li key={index}>    
+                                    <a className={item.cName} href={item.url}>
+                                        {item.tittle}
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+
+                </ul>
+            </nav>
+        )
+    }
+}
+
+
+export default Navbar

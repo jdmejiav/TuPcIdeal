22087757e845b7f10f25fd32a91e6154cbc8e011
Juan Diego <jdmejiav@eafit.edu.co>
2021-03-04 11:22:20 -0500

    App.js modificado

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,12 @@
 
 import React from 'react';
 import Registro from './pages/Registro';
-<<<<<<< HEAD
+
 import login from './pages/login';
-import Main from './pages/mainpage.jsx'
-=======
-import Main from './pages/mainpage.jsx';
->>>>>>> 7815d07fb82b7efe0379bd7fbb8d71c0dfa58744
+import Home from './pages/home.jsx'
+
 import NotFoundPage from './error/NotFoundPage.jsx';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import Home from './pages/home.jsx';
 
 
 function App() {
@@ -17,15 +14,10 @@ function App() {
     <BrowserRouter>
       <Switch>
           <Route exact path='/registro' component={Registro} />
-<<<<<<< HEAD
           <Route exact path='/login' component={login} />
-          <Route exact path='/' component={Main} />
-          <Route path="*" component={NotFoundPage} />
-=======
-          <Route path="/home" component={Main} />
           <Route exact path='/' component={Home} />
-          <Route path="*" component={NotFoundPage} />         
->>>>>>> 7815d07fb82b7efe0379bd7fbb8d71c0dfa58744
+          <Route path="*" component={NotFoundPage} />
+
       </Switch>
     </BrowserRouter>
   

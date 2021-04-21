3c64ffdc4d88ab3d537e5559f93710a1ad9e02c5
Esteban GT <egonzalezt@eafit.edu.co>
2021-04-21 12:57:59 -0500

    Db pc analysis integration, content based algorithm. -add new db for the content based algorithm to get data and process to get the recomendations. -content based recomendation algorithm for the project. -process forms json information to python data objets.

--- Backend/Forms/admin.py
+++ Backend/Forms/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from Forms.models import Recomendations
 # Register your models here.
+
+@admin.register(Recomendations)
+class Recomendations(admin.ModelAdmin):
+    list_display = ('CPU','GPU','RAM','SSD','HDD','CAPACIDAD','SELECCION','PRESUPUESTO')
